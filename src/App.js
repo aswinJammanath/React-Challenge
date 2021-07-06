305f4c9f1de1a7f1df167fe0a5b89c8a5209d129
aswinJammanath <aswinjammanath@gmail.com>
2021-07-06 23:30:33 +0530

    removed return for single line code in the map for the spread operator example

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import './css/App.css'
-import {useState} from 'react'
+import { useState } from 'react'
 import Counter from './Counter'
 import Employer from './Employer'
 
 function App() {
   const [count, setCount] = useState(0) //Destructuring; since the return of the fn is an array of two elements
   //let count = 0
-  const addCount = ()=>{
+  const addCount = () => {
     // count = count +1
-    setCount(count +1)
+    setCount(count + 1)
   };
   let obj = {   //for spread operator, we ca see it below like {...obj}, combining the following variables into one
     title: '1st Counter',
     count
   }
-  let emp=[
+  let emp = [
     {
-      name:'AJ',age:27
+      name: 'AJ', age: 27
     },
     {
-      name:'RVN',age:32
+      name: 'RVN', age: 32
     }
   ]
   return (
     <div>
-          {/* Never use addCount() */}
-      <button onClick = {addCount}>Click Me</button>
+      {/* Never use addCount() */}
+      <button onClick={addCount}>Click Me</button>
       {/* <h1 className='count'>Counter: {count}</h1> */}
       {/* <Counter title='1st Counter' count={count}/> */}
-      <Counter {...obj}/>
+      <Counter {...obj} />
       {
-        emp.map((object,index)=>{
-          return(
-            <Employer key={index} name={object.name} age={object.age} />
-          )
-        })
+        emp.map((object, index) =>
+          <Employer key={index} {...object} />
+        )
       }
     </div>
   );
